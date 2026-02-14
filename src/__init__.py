0c0c9f91af728fe111c859c2a0acf141be531c67
Tugrul Albas <117865403+TugrulAlb@users.noreply.github.com>
2026-02-15 00:24:18 +0300

    chore: Update project metadata and ownership
    - Update author information to TugrulAlb
    - Set version to 2.0.0
    - Document module structure and dependencies

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+"""
+3D Bin Packing Optimization Engine
+===================================
+
+NP-Hard 3D Bin Packing problemi için Genetik Algoritma tabanlı çözüm motoru.
+
+Alt Modüller:
+    - src.core      : Algoritma mantığı (GA, Fitness, Packing Engine)
+    - src.models    : Veri modelleri (Konteyner, Ürün)
+    - src.utils     : Yardımcı araçlar (Parser, Görselleştirme, Helpers)
+"""
+
+__version__ = "2.0.0"
+__author__ = "TugrulAlb"
